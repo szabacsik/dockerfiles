b063b1d7685543b3d5546c8649c842a273be7d0a
András Szabácsik <andras@szabacsik.com>
2019-05-29 16:09:40 +0200

    Update PHP error reporting config to display and log all error messages

--- 03_ubuntu_nginx_php-fpm_phalcon/entrypoint.sh
+++ 03_ubuntu_nginx_php-fpm_phalcon/entrypoint.sh
@@ -4,9 +4,14 @@ if [ ! -f /home/worker/volume/artifact/current/public/index.php ]; then
     echo -e "<?php\n\tphpinfo();" > /home/worker/volume/artifact/current/public/index.php
 fi
 mkdir -p /home/worker/volume/logs/supervisord/child
-mkdir -p /home/worker/volume/logs/php-fpm
+mkdir -p /home/worker/volume/logs/php
 mkdir -p /home/worker/volume/logs/nginx
 mkdir -p /home/worker/volume/run
+touch /home/worker/volume/logs/php/fpm-error-stdout-stderr.log
+touch /home/worker/volume/logs/php/error.log
+touch /home/worker/volume/logs/php/supervisor-php-fpm-stdout.log
+touch /home/worker/volume/logs/php/supervisor-php-fpm-stderr.log
+touch /home/worker/volume/logs/php/slow.log
 chown worker:worker /home/worker -R
-chmod 700 /home/worker
+chmod 700 /home/worker -R
 /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
